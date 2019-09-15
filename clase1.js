be80a5be653aeaed01b4daf46076a8caec503f3e
Angel Santillan <angelsb94@gmail.com>
2019-09-14 21:01:18 -0500

    fin de la clase variables

--- clase1.js
+++ clase1.js
@@ -4,3 +4,15 @@ console.log('Hola mundo JS')
 var nombre
 
 nombre = 'Sacha'
+var nombre2 = 'pedro'
+
+var apellido = 'santos'
+
+var nombre3 = 'angel' ; var apellido2= 'santillan'
+
+var edad = 28 
+
+console.log('hola ' + nombre + 'hola' + nombre2)
+console.log(nombre2 + ' ' + apellido )
+console.log(nombre3 + ' ' + apellido2)
+console.log( 'tengo '+ edad + ' ' + 'años')
