b42b5c66630ea57e996bbd73750e573294a3fa45
Angel Santillan <angelsb94@gmail.com>
2019-09-15 17:08:17 -0500

    Destructurar objetos

--- script.js
+++ script.js
@@ -14,9 +14,19 @@ var dario = {
 
 
 
-function imprimir_Mayusculas({ nombre }) {
+function imprimir_Mayusculas(persona) {
+    var { nombre } = persona
     console.log(nombre.toLocaleUpperCase())
 }
 
 imprimir_Mayusculas(sacha)
 imprimir_Mayusculas(dario)
+
+
+function imprimir_nombre_edad (persona) {
+    var {nombre,edad}=persona
+    console.log('Hola, me llamo ' + nombre + ' y tengo ' + edad + ' años')
+}
+
+imprimir_nombre_edad(sacha)
+imprimir_nombre_edad(dario)
