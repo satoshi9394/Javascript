77fa97f37a3ccb5f809fc97e3c5a333639ce0133
Angel Santillan <angelsb94@gmail.com>
2019-09-17 20:56:58 -0500

    Condisionales

--- script.js
+++ script.js
@@ -1,43 +1,64 @@
 
 var sacha = {
     nombre: 'Sacha',
-    apellido: 'Lista',
-    edad: 28,
+    apellido: 'Lopez',
+    edad: 10,
+    ingeniero : false,
+    cocinero: false,
+    cantante: true,
+    dj: false,
+    guitarrista: false,
+    drone: false,
 }
 
-var dario = {
-    nombre: 'Dario',
-    apellido: 'lopez',
-    edad: 30,
-}
 
-
-
-
-function imprimir_Mayusculas(persona) {
-    var { nombre } = persona
-    console.log(nombre.toLocaleUpperCase())
-}
-
-imprimir_Mayusculas(sacha)
-imprimir_Mayusculas(dario)
-
-
-function imprimir_nombre_edad (persona) {
-    var {nombre,edad}=persona
-    console.log('Hola, me llamo ' + nombre + ' y tengo ' + edad + ' años')
-}
-
-imprimir_nombre_edad(sacha)
-imprimir_nombre_edad(dario)
-
-
-function cumpleaños(persona) {
-    return {
-        ...persona,
-        edad: persona.edad + 1
+function imprimirProfesiones(persona) {
+    console.log(`${persona.nombre} es:`)
+    if (persona.ingeniero){
+        console.log('Ingeniero')
+    }else{
+        console.log('No es Ingeniero')
     }
+
+    if (persona.cocinero){
+        console.log('Cocinero')
+    }else{
+        console.log('No es Cocinero')
+    }
+    if (persona.cantante){
+        console.log('Cantante')
+    }else{
+        console.log('No es Cantante')
+    }
+    if (persona.dj){
+        console.log('DJ')
+    }else{
+        console.log('No es DJ')
+    }
+    if (persona.guitarrista){
+        console.log('Guitarrista')
+    }else{
+        console.log('No es Guitarrista')
+    }
+    if (persona.drone){
+        console.log('Vuela Drone')
+    }else{
+        console.log('No es piloto de Drone')
+    }
+    
     
 }
 
+imprimirProfesiones(sacha)
 
+
+function ImprimirSiEsMayorDeEdad(persona){
+    if(persona.edad >= 18){
+        console.log(`${persona.nombre} es mayor de edad por que tiene ${persona.edad} años`)
+    }else{
+        console.log(`${persona.nombre} es menor de edad por que tiene ${persona.edad} años`)
+    }
+}
+
+
+ImprimirSiEsMayorDeEdad(sacha)
