8d06544aecaae7979dc408c1ce51af6f857a26ae
Angel Santillan <angelsb94@gmail.com>
2019-09-24 18:58:07 -0500

    ciclos for

--- script.js
+++ script.js
@@ -2,84 +2,36 @@
 var sacha = {
     nombre: 'Sacha',
     apellido: 'Lopez',
-    edad: 28,
-    ingeniero : false,
-    cocinero: false,
-    cantante: true,
-    dj: false,
-    guitarrista: false,
-    drone: false,
+    edad: 25,
+    peso: 75,
 }
 
-var juan = {
-    nombre: 'Juan',
-    apellido: 'Guisardo',
-    edad: 10,
-}
+console.log(`Al inicio del año ${ sacha.nombre } peso ${ sacha.peso }kg`)
 
+const INCREMENTO_PESO = 0.2
 
-function imprimirProfesiones(persona) {
-    console.log(`${persona.nombre} es:`)
-    if (persona.ingeniero){
-        console.log('Ingeniero')
-    }else{
-        console.log('No es Ingeniero')
-    }
+const aumentarDePeso = persona => persona.peso += INCREMENTO_PESO
+const adelgazar = persona => persona.peso -= INCREMENTO_PESO
 
-    if (persona.cocinero){
-        console.log('Cocinero')
-    }else{
-        console.log('No es Cocinero')
-    }
-    if (persona.cantante){
-        console.log('Cantante')
-    }else{
-        console.log('No es Cantante')
-    }
-    if (persona.dj){
-        console.log('DJ')
-    }else{
-        console.log('No es DJ')
-    }
-    if (persona.guitarrista){
-        console.log('Guitarrista')
-    }else{
-        console.log('No es Guitarrista')
-    }
-    if (persona.drone){
-        console.log('Vuela Drone')
-    }else{
-        console.log('No es piloto de Drone')
-    }
-    
-    
-}
+for (var i = 1;  i<=365; i++ ) {
+    var random = Math.random()
 
-imprimirProfesiones(sacha)
-
-const MAYORIA_DE_EDAD = 18
-
-
-
-const esMayorDeEdad = ({ edad }) => edad >= MAYORIA_DE_EDAD
-
-const esMenorDeEdad = persona => !esMayorDeEdad(persona)
-
-function ImprimirSiEsMayorDeEdad(persona){
-    if(esMayorDeEdad(persona)){
-        console.log(`${persona.nombre} es mayor de edad por que tiene ${persona.edad} años`)
-    }else{
-        console.log(`${persona.nombre} es menor de edad por que tiene ${persona.edad} años`)
+    if (random< 0.25){
+        aumentarDePeso(sacha)
+    }else if (random < 0.5  ) {
+        adelgazar(sacha)
     }
 }
 
 
-ImprimirSiEsMayorDeEdad(sacha)
 
 
-function permitirAceso(persona){
-    if (!esMayorDeEdad(persona)){
-        console.log('ACCESO DENEGADO')
-    }
+console.log(`Al final del año ${ sacha.nombre } peso ${ sacha.peso.toFixed(1) }kg`)
+
+if (sacha.peso > 75 ) {
+    console.log(`${sacha.nombre} subio de peso`)
+}else if(sacha.peso == 75){
+    console.log(`${sacha.nombre} permanecio con el mismo peso`)
+}else{
+    console.log(`${sacha.nombre} bajo de peso`)
 }
-
