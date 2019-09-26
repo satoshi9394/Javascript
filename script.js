8724a68d23f5db4b164af176f922ea27a0213e34
Angel Santillan <angelsb94@gmail.com>
2019-09-25 21:11:11 -0500

    Estructuras repetitivas: do-while

--- script.js
+++ script.js
@@ -1,45 +1,17 @@
 
-var sacha = {
-    nombre: 'Sacha',
-    apellido: 'Lopez',
-    edad: 25,
-    peso: 75,
-}
-
-console.log(`Al inicio del año ${ sacha.nombre } peso ${ sacha.peso }kg`)
-
-const INCREMENTO_PESO = 0.3
-const PESO_INICIAL = sacha.peso
-const DIAS_DEL_AÑO = 365
-
-const aumentarDePeso = persona => persona.peso += INCREMENTO_PESO
-const adelgazar = persona => persona.peso -= INCREMENTO_PESO
-const comeMucho = () => Math.random() < 0.3
-const realizaDeporte = () => Math.random() < 0.4
+var contador = 0
 
 
-const META= sacha.peso - 3
-var dias = 0
+const llueve = () => Math.random() < 0.25
 
-while (sacha.peso > META ) {
-    if(comeMucho()) {
-        aumentarDePeso(sacha)
-    }
-    if(realizaDeporte()) {
-        adelgazar(sacha)
-    }
-    dias += 1
-}
+do {
+    contador++
+}while(!llueve())
 
 
-
-
-console.log(`Pasaron  ${ dias } dias hasta que ${ sacha.nombre} adelgazo 3kg`)
-
-if (sacha.peso > PESO_INICIAL ) {
-    console.log(`${sacha.nombre} subio de peso`)
-}else if(sacha.peso == PESO_INICIAL){
-    console.log(`${sacha.nombre} permanecio con el mismo peso`)
+if (contador===1){
+    console.log(`Fui a ver si llovia ${ contador } vez`)
 }else{
-    console.log(`${sacha.nombre} bajo de peso`)
+    console.log(`Fui a ver si llovia ${ contador } veces`)
 }
+
