a49923d09771b740a55e0f61c62ba040242b0168
Angel Santillan <angelsb94@gmail.com>
2019-09-29 18:19:40 -0500

    Reducir un array a un valor

--- script.js
+++ script.js
@@ -1,52 +1,60 @@
 var sasha = {
     nombre: 'Sacha',
     apellido: 'Lopez',
-    altura: 1.72
+    altura: 1.72,
+    cantidadDeLibros: 78
 }
 
 var pedro = {
     nombre: 'Pedro',
     apellido: 'Ortiz',
-    altura: 1.62
+    altura: 1.62,
+    cantidadDeLibros: 50
 }
 
 
 var juan = {
     nombre: 'Juan',
     apellido: 'Perez',
-    altura: 1.85
+    altura: 1.85,
+    cantidadDeLibros: 100
 }
 
 
 var alan = {
     nombre: 'Alan',
     apellido: 'Salto',
-    altura: 1.80
+    altura: 1.80,
+    cantidadDeLibros: 89
 }
 
 
 var ricardo = {
     nombre: 'Ricardo',
     apellido: 'Gonzales',
-    altura: 1.50
+    altura: 1.50,
+    cantidadDeLibros: 65
 }
 
 
 var angel = {
     nombre: 'Angel',
     apellido: 'Santillan',
-    altura: 1.75
+    altura: 1.75,
+    cantidadDeLibros: 47
 }
 
 
 var personas = [sasha , pedro, juan, alan, ricardo, angel]
 
-const pasarAlturaACms = persona => ({
-        ...persona,
-        altura: persona.altura * 100
-    })
-
-var personasCms = personas.map(pasarAlturaACms)
+const reducer = (acum, { cantidadDeLibros }) => 
+    acum + cantidadDeLibros
 
 
-console.log(personasCms)
+var totalDeLibros = personas.reduce(reducer, 0)
+
+
+
+
+
+console.log(`En total todos tienen ${totalDeLibros} libros`)
