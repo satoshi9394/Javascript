4059a2e8321167cbb1f9b0eb6ced3cd58b14d294
Angel Santillan <angelsb94@gmail.com>
2019-09-29 18:12:03 -0500

    arrays map

--- script.js
+++ script.js
@@ -1,17 +1,52 @@
-var signo = prompt('Cual es tu signo? ')
+var sasha = {
+    nombre: 'Sacha',
+    apellido: 'Lopez',
+    altura: 1.72
+}
+
+var pedro = {
+    nombre: 'Pedro',
+    apellido: 'Ortiz',
+    altura: 1.62
+}
 
 
+var juan = {
+    nombre: 'Juan',
+    apellido: 'Perez',
+    altura: 1.85
+}
 
-switch(signo){
-    case 'acuario':
-        console.log("Escogiste el primer signo")
-        break
-    case 'picisis':
-        console.log("Escogiste el Segundo signo")
-        break
-    case 'leo':
-        console.log("Escogiste el Tercero signo")
-        break
-    default:
-        console.log("aun no agregas ese signo o no es uno de ellos")
-}   
+
+var alan = {
+    nombre: 'Alan',
+    apellido: 'Salto',
+    altura: 1.80
+}
+
+
+var ricardo = {
+    nombre: 'Ricardo',
+    apellido: 'Gonzales',
+    altura: 1.50
+}
+
+
+var angel = {
+    nombre: 'Angel',
+    apellido: 'Santillan',
+    altura: 1.75
+}
+
+
+var personas = [sasha , pedro, juan, alan, ricardo, angel]
+
+const pasarAlturaACms = persona => ({
+        ...persona,
+        altura: persona.altura * 100
+    })
+
+var personasCms = personas.map(pasarAlturaACms)
+
+
+console.log(personasCms)
