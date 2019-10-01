89fe51fbeb2bf425c73f0fafbde7c243eae78271
Angel Santillan <angelsb94@gmail.com>
2019-09-30 20:59:59 -0500

    Modificar prototipo

--- script.js
+++ script.js
@@ -6,12 +6,20 @@ function Persona(nombre, apellido, altura) {
     this.altura = altura
 }
 
-Persona.prototype.saludar = function () {
+Persona.prototype.saludar = () => {
     console.log(`Hola me llamo ${this.nombre} ${this.apellido} `)
 }
+Persona.prototype.soyAlto = () => this.altura > 1.8
+
+
 
 var sasha = new Persona('Sacha', 'Lopez',1.75)
-sasha.saludar()
-var arturo = new Persona('Arturo', 'Santillan', 1.80)
+var arturo = new Persona('Arturo', 'Santillan', 1.89)
+var angel = new Persona('Angel', 'Gonzales', 1.79)
 
-arturo.saludar()
+
+sasha.soyAlto()
+arturo.soyAlto()
+angel.soyAlto()
+
+
