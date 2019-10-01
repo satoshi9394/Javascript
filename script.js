6076553bcca22069c4ec77e1715f750e4d79ec10
Angel Santillan <angelsb94@gmail.com>
2019-09-30 19:48:28 -0500

    prototipos seudo clases

--- script.js
+++ script.js
@@ -1,60 +1,17 @@
-var sasha = {
-    nombre: 'Sacha',
-    apellido: 'Lopez',
-    altura: 1.72,
-    cantidadDeLibros: 78
+
+
+function Persona(nombre, apellido, altura) {
+    this.nombre = nombre
+    this.apellido = apellido
+    this.altura = altura
 }
 
-var pedro = {
-    nombre: 'Pedro',
-    apellido: 'Ortiz',
-    altura: 1.62,
-    cantidadDeLibros: 50
+Persona.prototype.saludar = function () {
+    console.log(`Hola me llamo ${this.nombre} ${this.apellido} `)
 }
 
+var sasha = new Persona('Sacha', 'Lopez',1.75)
+sasha.saludar()
+var arturo = new Persona('Arturo', 'Santillan', 1.80)
 
-var juan = {
-    nombre: 'Juan',
-    apellido: 'Perez',
-    altura: 1.85,
-    cantidadDeLibros: 100
-}
-
-
-var alan = {
-    nombre: 'Alan',
-    apellido: 'Salto',
-    altura: 1.80,
-    cantidadDeLibros: 89
-}
-
-
-var ricardo = {
-    nombre: 'Ricardo',
-    apellido: 'Gonzales',
-    altura: 1.50,
-    cantidadDeLibros: 65
-}
-
-
-var angel = {
-    nombre: 'Angel',
-    apellido: 'Santillan',
-    altura: 1.75,
-    cantidadDeLibros: 47
-}
-
-
-var personas = [sasha , pedro, juan, alan, ricardo, angel]
-
-const reducer = (acum, { cantidadDeLibros }) => 
-    acum + cantidadDeLibros
-
-
-var totalDeLibros = personas.reduce(reducer, 0)
-
-
-
-
-
-console.log(`En total todos tienen ${totalDeLibros} libros`)
+arturo.saludar()
