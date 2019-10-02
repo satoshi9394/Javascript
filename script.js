e4d39d07127f2399de414197559ae6f8f5846803
Angel Santillan <angelsb94@gmail.com>
2019-10-01 20:30:58 -0500

    clases reciben funciones

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ class Persona{
         this.apellido = apellido
         this.altura = altura
     }
-    saludar() {
+    saludar(fn) {
         console.log(`Hola me llamo ${this.nombre} ${this.apellido} `)
+        if (fn) {
+            fn(this.nombre, this.apellido)
+        }
     }
     soyAlto() {
         return this.altura > 1.8
@@ -17,21 +20,30 @@ class Desarrollador extends Persona {
     constructor (nombre, apellido, altura) {
         super(nombre, apellido, altura)
     }
-    saludar() {
+    saludar(fn) {
         console.log(`Hola me llamo ${this.nombre} ${this.apellido} y soy desarrolldor/a `)
+        if (fn) {
+            fn(this.nombre, this.apellido, true)
+        }
+    }
+}
+
+function responderSaludo (nombre , apellido, esDev) {
+    console.log(`Buen dia ${nombre} ${apellido} `)
+    if (esDev) {
+        console.log(`Vaya no sabia que eras programador/a`)
     }
 }
 
 
 
 
-
-
-/* var sasha = new Persona('Sacha', 'Lopez',1.75)
+var sasha = new Persona('Sacha', 'Lopez',1.75)
 var arturo = new Persona('Arturo', 'Santillan', 1.89)
-var angel = new Persona('Angel', 'Gonzales', 1.79) */
+var angel = new Desarrollador('Angel', 'Gonzales', 1.79) 
 
-
-
+sasha.saludar()
+arturo.saludar(responderSaludo)
+angel.saludar(responderSaludo)
 
 
