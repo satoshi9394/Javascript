892f02239b9a51797de6f4f9317dd4dac84ca56d
Angel Santillan <angelsb94@gmail.com>
2019-10-01 21:14:12 -0500

    callbacks

--- script.js
+++ script.js
@@ -1,49 +1,12 @@
-class Persona{
-    constructor(nombre, apellido, altura) {
-        this.nombre = nombre
-        this.apellido = apellido
-        this.altura = altura
-    }
-    saludar(fn) {
-        console.log(`Hola me llamo ${this.nombre} ${this.apellido} `)
-        if (fn) {
-            fn(this.nombre, this.apellido)
-        }
-    }
-    soyAlto() {
-        return this.altura > 1.8
-    }
+
+const API_URL = 'https://swapi.co/api/'
+const PEOPLE_URL = 'people/:id'
+
+const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id', 1)}` 
+const opts =  { crossDomain: false }
+
+const onRespose = function(luke) {
+    console.log(`hola yo soy ${luke.name}`)
 }
 
-
-class Desarrollador extends Persona {
-    constructor (nombre, apellido, altura) {
-        super(nombre, apellido, altura)
-    }
-    saludar(fn) {
-        console.log(`Hola me llamo ${this.nombre} ${this.apellido} y soy desarrolldor/a `)
-        if (fn) {
-            fn(this.nombre, this.apellido, true)
-        }
-    }
-}
-
-function responderSaludo (nombre , apellido, esDev) {
-    console.log(`Buen dia ${nombre} ${apellido} `)
-    if (esDev) {
-        console.log(`Vaya no sabia que eras programador/a`)
-    }
-}
-
-
-
-
-var sasha = new Persona('Sacha', 'Lopez',1.75)
-var arturo = new Persona('Arturo', 'Santillan', 1.89)
-var angel = new Desarrollador('Angel', 'Gonzales', 1.79) 
-
-sasha.saludar()
-arturo.saludar(responderSaludo)
-angel.saludar(responderSaludo)
-
-
+$.get(lukeUrl , opts , onRespose )
