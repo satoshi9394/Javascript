a6b427ff1c4e50c1dbcdc017a07d34457c6c575a
Angel Santillan <angelsb94@gmail.com>
2019-10-02 17:39:38 -0500

    Casos donde existan errores con callbacks

--- script.js
+++ script.js
@@ -5,18 +5,38 @@ const PEOPLE_URL = 'people/:id'
 /* const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id', 1)}`   cuando solo querias traer uno xd*/
 const opts =  { crossDomain: true }
 
-const onRespose = function(persona) {
-    console.log(`Hola, yo soy ${persona.name}`)
-}
 
 
-function obtenerPersonaje(id) {
+
+function obtenerPersonaje(id, callback) {
     const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
-    $.get(url , opts , onRespose )
+
+    $.get(url , opts , callback).fail(function() {
+        console.log(`Sucedio un error interno en el servidor.
+        No se pudo obtener la respuesta id:${id} `)
+    })
 }
 
-obtenerPersonaje(1)
-obtenerPersonaje(2)
-obtenerPersonaje(3)
+obtenerPersonaje(1, function (personaje)  {
+    console.log(`Hola, yo soy ${personaje.name}`)
+
+    obtenerPersonaje(2,function(personaje) {
+        console.log(`Hola, yo soy ${personaje.name}`)
+
+        obtenerPersonaje(3, function(personaje) {
+            console.log(`Hola, yo soy ${personaje.name}`)
+
+            obtenerPersonaje(4, function(personaje) {
+                console.log(`Hola, yo soy ${personaje.name}`)
+
+                obtenerPersonaje(5, function(personaje) {
+                    console.log(`Hola, yo soy ${personaje.name}`)
+                })
+            })
+        })
+    })
+})
+
+
 
 
