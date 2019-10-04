046468be45359bc17776aa7cbf55fad37c9c83eb
Angel Santillan <angelsb94@gmail.com>
2019-10-03 20:55:21 -0500

    asyc and await

--- script.js
+++ script.js
@@ -23,21 +23,19 @@ function onError(id) {
     console.log(`El personaje con Id:${id} no se pudo obtener `)
 }
 
+async function obtenerPersonajes() {
+    var ids = [1,2,3,4,5,6,7]
+    var promesas = ids.map( id => obtenerPersonaje(id))
+    try{
+        var personajes = await Promise.all(promesas)
+        .then(personajes => console.log(personajes))
+    }catch(id){
+        onError(id)
+    }
+}
 
 
-var ids = [1,2,3,4,5,6,7]
-/* var promesas = ids.map(function (id) {
-    return obtenerPersonaje(id)
-}) */
-
-var promesas = ids.map( id => obtenerPersonaje(id))
-
-Promise
-    .all(promesas)
-    .then(personajes => console.log(personajes))
-    .catch(onError)
-
-
+obtenerPersonajes()
 
 
 
