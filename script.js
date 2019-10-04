394a95dd68dd9924ecb22955c5e18ace16d1561c
Angel Santillan <angelsb94@gmail.com>
2019-10-03 21:13:36 -0500

    comenzar juego

--- script.js
+++ script.js
@@ -1,73 +1,31 @@
 
-const API_URL = 'https://swapi.co/api/'
-const PEOPLE_URL = 'people/:id'
 
-/* const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id', 1)}`   cuando solo querias traer uno xd*/
-const opts =  { crossDomain: true }
+const celeste = document.getElementById('celeste')
+const violeta = document.getElementById('violeta')
+const naranja = document.getElementById('naranja')
+const verde   = document.getElementById('verde')
+const btnEmpezar = document.getElementById('btnEmpezar')
 
 
 
-
-function obtenerPersonaje(id) {
-    return new Promise( function (resolve , reject) {
-        const url = `${API_URL}${PEOPLE_URL.replace(':id', id) }`
-
-        $.get(url , opts , function(data) {
-            resolve(data)
-        })
-        .fail( () => reject(id) )
-    })
-}
-
-function onError(id) {
-    console.log(`El personaje con Id:${id} no se pudo obtener `)
-}
-
-async function obtenerPersonajes() {
-    var ids = [1,2,3,4,5,6,7]
-    var promesas = ids.map( id => obtenerPersonaje(id))
-    try{
-        var personajes = await Promise.all(promesas)
-        .then(personajes => console.log(personajes))
-    }catch(id){
-        onError(id)
+class Juego {
+    constructor() {
+        this.inicializar()
     }
+
+    inicializar() {
+        btnEmpezar.classList.add('hide')
+    }
+
+
+
+
 }
 
 
-obtenerPersonajes()
 
-
-
-
-
-
-
-
-
-
-
-/* obtenerPersonaje(1)
-    .then( (personaje) => {
-        console.log(`El personaje 1 es: ${ personaje.name }`)
-        return obtenerPersonaje(2)
-    })
-    .then( (personaje) => {
-        console.log(`El personaje 2 es: ${ personaje.name }`)
-        return obtenerPersonaje(3)
-    })
-    .then( (personaje) => {
-        console.log(`El personaje 3 es: ${ personaje.name }`)
-    })
-    .catch( (id) => {
-        onError(id)
-    }) */
-
-
-
-
-
-
-
+function empezarJuego() {
+    var juego = new Juego()
+}
 
 
